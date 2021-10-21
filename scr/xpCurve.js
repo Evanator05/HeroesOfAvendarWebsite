f6cf39a6835854829dd5a6184da7dbd90ba29dba
Evanator05 <watsonevan2005@gmail.com>
2021-10-21 01:13:40 -0600

    added graph

--- scr/xpCurve.js
+++ scr/xpCurve.js
@@ -1,6 +1,34 @@
-function pressedButton() {
-  let level = document.getElementById("level").value
-  document.getElementById("ans").innerHTML = 70 + (2*((level-1)*(level-1)))
+function main() {
+  pressedButton();
+  drawGraph();
 }
 
-pressedButton()
+function drawGraph() {
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+
+  var width = canvas.width
+  var height = canvas.height
+
+  ctx.fillStyle="white"
+  ctx.fillRect(0, 0, width, height);
+
+  var steps = 20
+  var widthsteps = width/steps
+  ctx.scale(1, -1);
+  ctx.beginPath();
+  ctx.moveTo(0,0);
+  for (let i = 0; i < steps; i++) {
+    //plot points based on this quadradic 2(level)^2+70
+    ctx.lineTo((widthsteps*i),70 + (2*((i-1)*(i-1)))-height)
+  }
+  ctx.stroke();
+
+}
+
+function pressedButton() {
+  let level = document.getElementById("level").value;
+  document.getElementById("ans").innerHTML = 70 + (2*((level-1)*(level-1)));
+}
+
+main()
